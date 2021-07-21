8cec28eba8e70ffe3fdb0379e78a96f2ce99e96e
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-07-21 12:26:46 +0200

    Optimized and validated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,36 @@
+// Four functions for settings the colors according to the balls
 function changeColors1() {
-    // console.log('changeColors1()');
     setColors("#1d8f53", "#6e0e60");
 }
-
 function changeColors2() {
-    // console.log('changeColors2()');
     setColors("#454270", "#d9ab42");
 }
-
 function changeColors3() {
-    // console.log('changeColors3()');
     setColors("#00011f", "#c65d0c");
 }
-
 function changeColors4() {
-    // console.log('changeColors4()');
     setColors("#0b0e69", "#2accfe");
 }
 
+// the last ball is specific, it generates random colors for each click
 function changeColorsRandom() {
-    console.log('changeColorsRandom()');
-
+    // get some random colors for background and text-color
     let fgColor = getRandomColor();
     let bgColor = getRandomColor();
 
-    document.getElementById("random1").style.color = fgColor;
-    document.getElementById("random2").style.color = bgColor;
-    document.getElementById("ball5").style.background = "linear-gradient(45deg, " + bgColor + " 0 50%, " + fgColor + " 50% 100%)";
+    // fill the ball with the random colors
+    document.getElementById("ball5").style.background = "linear-gradient(45deg, " + bgColor + " 50%, " + fgColor + " 50%)";
 
     setColors(bgColor, fgColor);
 }
 
+// apply the colors to headline and body background
 function setColors(bgColor, fgColor) {
-    document.body.style.color = fgColor;
+    document.getElementById("headline").style.color = fgColor;
     document.body.style.backgroundColor = bgColor;
 }
 
+// create a random color by using random numbers for each single color
 function getRandomColor() {
     let r = getRandomInt(0, 255);
     let g = getRandomInt(0, 255);
@@ -43,6 +38,7 @@ function getRandomColor() {
     return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
+// create a random number between min and max
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
